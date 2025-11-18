8f3b509a4313c95b2abaa10a8640eccdba3eacb8
MiguelMatos-10 <miguelmendoncamatos@gmail.com>
2025-11-18 01:29:42 +0100

    Coisas

--- notebooks_2nd_delivery/Preprocessing_pipelines/Outliers_pipeline.py
+++ notebooks_2nd_delivery/Preprocessing_pipelines/Outliers_pipeline.py
@@ -9,14 +9,16 @@ from sklearn.neighbors import LocalOutlierFactor
 class OutliersDealer(BaseEstimator, TransformerMixin):
 
     def __init__(self, 
-                 outlier_method = "Z_Score", # the default method
+                 outlier_method = "z-score", # the default method
                  mod_outliers_cols = [],
                  sev_outliers_cols = [],
                  threshold=3, # Pick 2 or 3 as the threshold value of "z"
+                 z_columns = [],
                  contamination_IF=0.05, 
                  random_state=42,
                  n_neighbors=20, 
-                 contamination_LOF=0.05,
+                 contamination_LOF= 0.05,
+                 model_columns = [], #columns selected for IF model or LOF model
                  **kwargs
                  ):
         
@@ -28,14 +30,17 @@ class OutliersDealer(BaseEstimator, TransformerMixin):
 
         # Z-Score method parameters
         self.threshold = threshold
+        self.z_columns = z_columns
 
         # Isolation Forest method parameters
         self.contamination_IF = contamination_IF
         self.random_state = random_state
+        self.model_columns = model_columns
 
         # Local Outliers Factor method parameters
         self.n_neighbors = n_neighbors
         self.contamination_LOF = contamination_LOF
+        self.model_columns = model_columns
 
 
     def fit(self, X_train, **kwargs):
@@ -43,22 +48,35 @@ class OutliersDealer(BaseEstimator, TransformerMixin):
         # Interquartil Range Method
         if self.outlier_method == "IQR":
 
-            self.q1_ = X_train.quantile(0.25)
-            self.q3_ = X_train.quantile(0.75)
-            self.iqr_ = self.q3_ - self.q1_
+            self.q1_ = {}
+            self.q3_ = {}
+            self.iqr_ = {}
 
+            for col in set(self.mod_outliers_cols + self.sev_outliers_cols):
+                q1 = X_train[col].quantile(0.25)
+                q3 = X_train[col].quantile(0.75)
+                iqr = q3 - q1
+
+                self.q1_[col] = q1
+                self.q3_[col] = q3
+                self.iqr_[col] = iqr
         
-        elif self.outlier_method == "Z_Score":
-            self.means_ = X_train.mean()
-            self.stds_ = X_train.std()
+        elif self.outlier_method == "z-score":
+
+            self.means_ = {}
+            self.stds_ = {}
+
+            for col in self.z_columns:
+                self.means_[col] = X_train[col].mean()
+                self.stds_[col] = X_train[col].std()
 
         elif self.outlier_method == "Isolation_Forest":
 
             self.model = IsolationForest(
-            contamination=self.contamination,
+            contamination=self.contamination_IF,
             random_state=self.random_state
             )
-            self.model.fit(X_train)
+            self.model.fit(X_train[self.model_columns])
 
         elif self.outlier_method == "LOF":
             self.model = LocalOutlierFactor(
@@ -66,11 +84,11 @@ class OutliersDealer(BaseEstimator, TransformerMixin):
             contamination=self.contamination_LOF,
             novelty=True  # required for transform()
             )
-            self.model.fit(X_train)
+            self.model.fit(X_train[self.model_columns])
 
         return self
     
-    def transform(self, X, y, **kwargs):
+    def transform(self, X, **kwargs):
 
 
         X = X.copy()
@@ -82,25 +100,40 @@ class OutliersDealer(BaseEstimator, TransformerMixin):
             if len(self.mod_outliers_cols) > 0:
                 for col in self.mod_outliers_cols:
 
-                    X[col] = np.clip(X[col], self.q1_[col] - 1.5 * self.iqr_[col] , self.q3_[col] + 1.5 * self.iqr_[col])
+                    X[col] = np.clip(X[col], 
+                                     self.q1_[col] - 1.5 * self.iqr_[col], 
+                                     self.q3_[col] + 1.5 * self.iqr_[col])
 
             #Capping severate outliers        
             if len(self.sev_outliers_cols) > 0:    
                 for col in self.sev_outliers_cols:
 
-                    X[col] = np.clip(X[col], self.q1_[col] - 3 * self.iqr_[col] , self.q3_[col] + 3 * self.iqr_[col])
+                    X[col] = np.clip(X[col], 
+                                     self.q1_[col] - 3 * self.iqr_[col],
+                                     self.q3_[col] + 3 * self.iqr_[col])
             
-            return X, y
+            return X
                 
         
-        elif self.outlier_method == "Z-score":
+        elif self.outlier_method == "z-score":
 
             z = (X - self.means_) / self.stds_
             mask = (np.abs(z) < self.threshold).all(axis=1)
             return X[mask], y[mask]
         
+            #z_mask = np.ones(len(X), dtype=bool)
+
+            #for col in self.z_columns:
+            #    z_scores = (X[col] - self.means_[col]) / self.stds_[col]
+            #    z_mask &= (np.abs(z_scores) < self.threshold)
+
+            # Filter rows based on outlier mask
+            #return X[z_mask].reset_index(drop=True)
+
+            # NEED TO LOOK AT THE ABOVE COMMENTED CODE AND SEE IF MAKES SENSE AND SHOULD IMPLEMENT IT
+        
         elif self.outlier_method in ["Isolation_Forest", "LOF"]:
-            preds = self.model.predict(X)  # +1 = normal, -1 = outlier
+            preds = self.model.predict(X[self.model_columns])  # +1 = normal, -1 = outlier
             mask = preds == 1
             
             return X[mask], y[mask]
---
+++ notebooks_2nd_delivery/Preprocessing_pipelines/Scaling_pipeline.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+
+
+class ScalingDealer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, scaler_name="robust", **kwargs):
+        self.scaler_name = scaler_name.lower()
+        self.kwargs = kwargs
+
+    def fit(self, X, y=None):
+        scalers = {
+            "robust": RobustScaler,
+            "minmax": MinMaxScaler,
+            "standard": StandardScaler
+        }
+
+        if self.scaler_name not in scalers:
+            raise ValueError(
+                f"Invalid scaler_name '{self.scaler_name}'. "
+            )
+
+        self.scaler_ = scalers[self.scaler_name](**self.kwargs).fit(X)
+        return self
+
+    def transform(self, X, y=None):
+        
+        X = X.copy()
+        return self.scaler_.transform(X)
