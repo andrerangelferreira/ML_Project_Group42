9ea6ffb5865dcc7f6052e9b3e676688a6d2f344f
MiguelMatos-10 <miguelmendoncamatos@gmail.com>
2025-12-08 21:43:52 +0000

    Scaler update

--- notebooks_2nd_delivery/Preprocessing_pipelines/Scaling_pipeline.py
+++ notebooks_2nd_delivery/Preprocessing_pipelines/Scaling_pipeline.py
@@ -9,10 +9,12 @@ class ScalingDealer(BaseEstimator, TransformerMixin):
 
     def __init__(self, 
                  scaler_name="robust", 
+                 one_hot = False,
                  **kwargs
                  ):
 
         self.scaler_name = scaler_name
+        self.one_hot = one_hot
 
     def fit(self, X, **kwargs):
 
@@ -22,20 +24,37 @@ class ScalingDealer(BaseEstimator, TransformerMixin):
             "standard": StandardScaler
         }
 
-        self.cols_to_scale_ = ["car_age", "mileage", "tax", "mpg", "engineSize", "previousOwners"]
+        if self.one_hot == True:
+            
+            self.cols_to_scale_ = ["car_age", "mileage", "tax", "mpg", "engineSize", "previousOwners"]
 
-        self.scaler_ = scalers[self.scaler_name]().fit(X[self.cols_to_scale_])
+            self.scaler_ = scalers[self.scaler_name]().fit(X[self.cols_to_scale_])
+
+        else:
+
+            self.scaler_ = scalers[self.scaler_name]().fit(X)
+
         return self
 
     def transform(self, X, **kwargs):
         
         X = X.copy()
 
-        X_cols = X[self.cols_to_scale_]
-        X_encoded = X[[col for col in X.columns if col not in self.cols_to_scale_]]
+        if self.one_hot == True:
 
-        X_scaled = self.scaler_.transform(X_cols)
+            X_cols = X[self.cols_to_scale_]
+            X_encoded = X[[col for col in X.columns if col not in self.cols_to_scale_]]
 
-        X_scaled = pd.DataFrame(X_scaled, columns= X_cols.columns, index= X_cols.index)
+            X_scaled = self.scaler_.transform(X_cols)
+
+            X_scaled = pd.DataFrame(X_scaled, columns= X_cols.columns, index= X_cols.index)
+            
+            return pd.concat([X_scaled, X_encoded], axis=1)
         
-        return pd.concat([X_scaled, X_encoded], axis=1)
+        else:
+
+            X_scaled = self.scaler_.transform(X)
+
+            X_scaled = pd.DataFrame(X_scaled, columns= X.columns, index= X.index)
+            
+            return X_scaled
