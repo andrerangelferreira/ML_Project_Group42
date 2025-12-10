830cf6abdec5741b2c38cb2cf5e5f013a9264b81
andremaianicolau-git <andremaianicolau@gmail.com>
2025-12-10 16:28:08 +0000

    Documentação 1

--- notebooks_2nd_delivery/Feature selection.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.impute import SimpleImputer, KNNImputer
-from sklearn.impute import IterativeImputer
-from sklearn.preprocessing import StandardScaler
